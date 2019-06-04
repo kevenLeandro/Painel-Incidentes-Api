3d1b2bec7d84ef1133b7e65d405b1462c0e2eefa
Keven Leandro dos Santos <keven.santos@engdb.com.br>
2019-06-04 11:06:16 -0300

    html index,node , dockerfile

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
+
+router.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+  //__dirname : It will resolve to your project folder.
+});
+
+
+//add the router
+app.use('/', router);
+
+
+app.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname + '/'));
+
+//Launch listening server on port 8081  
+app.listen(8081, function () {  
+    console.log('app listening on port 8081!')  
+  })
+
+console.log('Running at Port 4000');
---
+++ js/script.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+   
+    assignDataToTable();
+
+    function assignDataToTable() {
+        $("tbody").empty();
+        $.ajax({    
+          type:"GET",
+          contentType: "application/json",
+          url:"http://localhost:8080/im-day",
+          success: function(data) {
+            var users = JSON.parse(JSON.stringify(data));
+            for (var i in users) {
+                $("tbody").
+                append("<tr> \
+                            <td>" + "</td> \
+                            <td>" +  users[i].name + "</td> \
+                            <td>" +  users[i].solved_day + "</td> \
+                            <td> \ </td> \
+                        </tr>");
+            }
+          },
+          error: function(data) { 
+            console.log(data);
+            }
+        });
+       
+    }
+
+});
